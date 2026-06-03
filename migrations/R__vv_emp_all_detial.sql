9ee2ee2ae74157d57575dadf4bd312f31d221675
JayaSudhakarAnde <sudhakar1007.ande@gmail.com>
2026-06-03 11:07:18 +0530

    Update and rename R__vv_emp_detial.sql to R__vv_emp_all_detial.sql

--- migrations/R__vv_emp_all_detial.sql
+++ migrations/R__vv_emp_all_detial.sql
@@ -1,4 +1,4 @@
-drop view if exists vv_emp_detail;
+drop view if exists vv_emp_all_detail;
 create view vv_emp_detail
 AS
 select emp_id, emp_name, doj, salary_lpa, role_id, created_on from tbl_employee;
