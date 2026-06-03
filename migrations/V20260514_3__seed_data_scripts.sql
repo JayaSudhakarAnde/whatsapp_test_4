90eb9d870bd232d6f8b1be2180723a9c5689d7e0
JayaSudhakarAnde <sudhakar1007.ande@gmail.com>
2026-06-03 11:31:31 +0530

    Update V20260514_3__seed_data_scripts.sql

--- migrations/V20260514_3__seed_data_scripts.sql
+++ migrations/V20260514_3__seed_data_scripts.sql
@@ -1,4 +1,4 @@
-insert into tbl_employee (emp_id, emp_name, doj, salary_lpa, gender, created_on)
+insert into tbl_employee (emp_id, emp_name, doj, salary_lpa, role_id, created_on)
 values ('VMIND01', 'Siva', NOW(), 1000000, 1, NOW())
   , ('VMIND02', 'Venkat', NOW(), 2000000, 2, NOW())
   , ('VMIND03', 'Raghava', NOW(), 1000000, 2, NOW())
