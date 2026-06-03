8faa9375ebbd5c79a5b08dbba23062b1ed0ef1c9
JayaSudhakarAnde <sudhakar1007.ande@gmail.com>
2026-06-03 11:20:02 +0530

    Update V20260510_1__create_table_scripts.sql

--- migrations/V20260510_1__create_table_scripts.sql
+++ migrations/V20260510_1__create_table_scripts.sql
@@ -1,1 +1,1 @@
-create table if not exists tbl_role (id int primary key auto_increment, role_code varchar(5), role_name varchar(50));
+create table if not exists tbl_project (id int primary key auto_increment, project_name varchar(100), created_on datetime);
