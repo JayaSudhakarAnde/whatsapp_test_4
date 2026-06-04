b632288f5468cd5d193b971084228d17e1044a47
JayaSudhakarAnde <sudhakar1007.ande@gmail.com>
2026-06-04 13:50:06 +0530

    Add files via upload

--- migrations/repeatable_migrations/R__vv_emp_basic_detail.sql
+++ migrations/repeatable_migrations/R__vv_emp_basic_detail.sql
@@ -1,4 +1,4 @@
 drop view if exists vv_emp_basic_detail;
 create view vv_emp_basic_detail
 AS
-select emp_id, emp_name, doj from tbl_employee;
+select emp_id, emp_name, doj, created_on from tbl_employee;
