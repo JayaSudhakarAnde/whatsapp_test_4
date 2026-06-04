46be631e7bcd6fbf4b29512b22ffe8fd1276000d
JayaSudhakarAnde <sudhakar1007.ande@gmail.com>
2026-06-04 13:50:31 +0530

    Add files via upload

---
+++ migrations/versioned_migrations/V20260515_1__seed_data_scripts.sql
@@ -0,0 +1,5 @@
+insert into tbl_employee (emp_id, emp_name, doj, salary_lpa, role_id, created_on)
+values ('VMIND01', 'Siva', NOW(), 1000000, 1, NOW())
+  , ('VMIND02', 'Venkat', NOW(), 2000000, 2, NOW())
+  , ('VMIND03', 'Raghava', NOW(), 1000000, 2, NOW())
+  , ('VMIND04', 'Shankar', NOW(), 3000000, 1, NOW());
